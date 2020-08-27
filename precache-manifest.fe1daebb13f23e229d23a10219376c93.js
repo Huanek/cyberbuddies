629fe3c1e09d36156d9841f4b65ec865abeae587
s16717 <56193797+s16717@users.noreply.github.com>
2020-08-27 15:39:52 +0200

    Updates

---
+++ precache-manifest.fe1daebb13f23e229d23a10219376c93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca5bd414af764d56a35fb4193e09ddc",
+    "url": "/cyberbuddies/index.html"
+  },
+  {
+    "revision": "e7571135bf04234ea55c",
+    "url": "/cyberbuddies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f83cc6bfdce6d9446a48",
+    "url": "/cyberbuddies/static/css/main.d70499d8.chunk.css"
+  },
+  {
+    "revision": "e7571135bf04234ea55c",
+    "url": "/cyberbuddies/static/js/2.a31a4944.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cyberbuddies/static/js/2.a31a4944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83cc6bfdce6d9446a48",
+    "url": "/cyberbuddies/static/js/main.b8c8d114.chunk.js"
+  },
+  {
+    "revision": "486739d65927b2c36c04",
+    "url": "/cyberbuddies/static/js/runtime-main.b926a093.js"
+  },
+  {
+    "revision": "c6cddf1504798057cfee7990065c7270",
+    "url": "/cyberbuddies/static/media/Robotica-ZYmB.c6cddf15.woff"
+  }
+]);
